1840ce149f79a95095a871334115df003851d13b
Nhan <nhan@iMac-Pro.local>
2023-03-11 12:26:49 +0700

    update

--- src/components/Admin/Content/HomePage/HomeContentAdmin.js
+++ src/components/Admin/Content/HomePage/HomeContentAdmin.js
@@ -2,13 +2,106 @@ import React from 'react'
 
 export default function HomeContentAdmin() {
   return (
-    <div className='pl-[235px] items-center'>
-        <div className='audio-tracks'>
-
+    <div className='pl-[235px] w-full flex'>
+      <div className='audio-tracks w-[60%] px-5'>
+        <div className='p-5 border w-full h-full bg-gradient-to-br from-[#555555] to-primary rounded-3xl shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)]'>
+          <h3 className='text-[#fff] text-lg text-left mb-4 font-CircularBold'>Danh sách bài hát</h3>
+          <table className='table w-full'>
+            <thead>
+              <tr className='font-CircularMedium text-left border-b text-[#fff]'>
+                <td className='leading-10'>#</td>
+                <td>Tên bài hát</td>
+                <td>Nghệ sĩ</td>
+                <td>Ngày đăng</td>
+              </tr>
+            </thead>
+            <tbody className='font-CircularLight text-[#fff]'>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div className='users-playlists'>
-
+      </div>
+      <div className='users-playlists w-[40%] px-5'>
+        <div className='p-5 bg-gradient-to-br rounded-3xl mb-5 from-[#555555] to-red-600'>
+          <h3 className='text-[#fff] text-lg text-left font-CircularBold'>Danh sách người dùng</h3>
+          <table className='table w-full'>
+            <thead>
+              <tr className='font-CircularMedium text-left border-b text-[#fff]'>
+                <td className='leading-10'>#</td>
+                <td>Tên bài hát</td>
+                <td>Nghệ sĩ</td>
+                <td>Ngày đăng</td>
+              </tr>
+            </thead>
+            <tbody className='font-CircularLight text-[#fff]'>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+        <div className='p-5 bg-gradient-to-br rounded-3xl from-[#555555] to-yellow-600'>
+          <h3 className='text-[#fff] text-lg text-left mb-4 font-CircularBold'>Danh sách playlists</h3>
+          <table className='table w-full'>
+            <thead>
+              <tr className='font-CircularMedium text-left border-b text-[#fff]'>
+                <td className='leading-10'>#</td>
+                <td>Tên bài hát</td>
+                <td>Nghệ sĩ</td>
+                <td>Ngày đăng</td>
+              </tr>
+            </thead>
+            <tbody className='font-CircularLight text-[#fff]'>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+              <tr className='border-b hover:underline hover:cursor-pointer border-[#ccc] text-left '>
+                <td>1</td>
+                <td className='leading-9'>Chiều hôm ấy</td>
+                <td>Jikay</td>
+                <td>20/8/1998</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Admin/Header/HomePage/HomeHeaderAdmin.js
+++ src/components/Admin/Header/HomePage/HomeHeaderAdmin.js
@@ -2,25 +2,31 @@ import React from 'react'
 import backgroundUsers from '../../../../assets/image/backgroundUsers.jpeg'
 export default function HomeHeaderAdmin() {
   return (
-    <div className='pl-[235px]'>
+    <div className='pl-[235px] pb-7'>
       <div className=''>
         <img className='max-h-96 w-full object-cover' src={backgroundUsers} alt="" />
       </div>
       <div className='p-7 absolute right-0 left-[235px] top-[64px]'>
         <h3 className='text-3xl text-[#fff] font-CircularBold tracking-widest uppercase text-left mb-12'>Dashboard</h3>
         <div className='grid grid-cols-3 gap-10'>
-          <div className='border-none text-[#fff] rounded-3xl shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#555555] to-[#2d2d2d] p-7'>
-            <p className='text-left'>Danh sách người dùng</p>
-            <h3 className='text-6xl text-right'>20</h3>
-          </div>
-          <div className='border-none text-[#fff] rounded-3xl shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#555555] to-[#2d2d2d] p-7'>
-            <p className='text-left'>Danh sách bài hát</p>
-            <h3 className='text-6xl text-right'>20</h3>
-          </div>
-          <div className='border-none text-[#fff] rounded-3xl shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#555555] to-[#2d2d2d] p-7'>
-            <p className='text-left'>Danh sách playlist</p>
-            <h3 className='text-6xl text-right'>20</h3>
-          </div>
+          <a href="">
+            <div className='border-none text-[#fff] rounded-3xl hover:scale-105 transition-all duration-200 shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#555555] to-red-600 p-7'>
+              <p className='text-left'>Danh sách người dùng</p>
+              <h3 className='text-6xl text-right'>20</h3>
+            </div>
+          </a>
+          <a href="">
+            <div className='border-none text-[#fff] rounded-3xl hover:scale-105 transition-all duration-200 shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#555555] to-primary p-7'>
+              <p className='text-left'>Danh sách bài hát</p>
+              <h3 className='text-6xl text-right'>20</h3>
+            </div>
+          </a>
+          <a href="">
+            <div className='border-none text-[#fff] rounded-3xl hover:scale-105 transition-all duration-200 shadow-[0_4px_60px_-15px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#555555] to-yellow-500 p-7'>
+              <p className='text-left'>Danh sách playlist</p>
+              <h3 className='text-6xl text-right'>20</h3>
+            </div>
+          </a>
         </div>
       </div>
     </div>
